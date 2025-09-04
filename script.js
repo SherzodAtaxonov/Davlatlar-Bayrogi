c621e19043cb19ec7d3d429d332aff700337b35c
Sherzod <ataxonovsherzod77@gmail.com>
2025-09-04 15:34:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,18 @@ let inp =document.getElementById('inp')
 let  btn =document.getElementById('btn')
 
 let request =new XMLHttpRequest();
-
-
 request.addEventListener('readystatechange', ()=>{
-    
     let jsn =JSON.parse(request.responseText)
-    
-    
-    
-    
-    
-    
-
 if(request.readyState == 1){
     h1.classList.remove('hidden')
 }
 else if(request.readyState == 4){
     h1.classList.add('hidden')
-    
         jsn.forEach((itn)=>{
             text.innerHTML += `<li class="li">
             <img src=${itn.flags.png} />
             <h2>${itn.name.common}</h2>
             </li>`
-
         })
     }
      inp.addEventListener('input',() => {
@@ -38,13 +26,9 @@ else if(request.readyState == 4){
                 itm.classList.remove('hidden')
             }else{
                 itm.classList.add('hidden')
-                
             }
         })
-
-           
     })
     })
 request.open('GET','https://restcountries.com/v3.1/all?fields=name,flags')
-
 request.send()
